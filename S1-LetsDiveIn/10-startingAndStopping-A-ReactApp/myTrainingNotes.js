d2ddb0a6fd5a0f4b8823dae0baf09ff1ab878135
VanessaTsang888 <vantaidesign8@gmail.com>
2021-03-02 20:53:05 +0000

    Completed a number of lectures within Section 02

---
+++ S1-LetsDiveIn/10-startingAndStopping-A-ReactApp/myTrainingNotes.js
@@ -0,0 +1,19 @@
+/**************************************************************************************************************************************************************** 
+
+10. Starting and Stopping a React App
+
+In my terminal:
+navigate into L10 > jsx foler: use this command: npm start > localhost:3000
+
+Now i have my react-app being ran inside of terminal so I can visit: localhost:3000
+To stop my react-app running: ctrl + c
+To start the app again, in the project directory, use this command: npm start
+
+Errors that I may get:
+Port in use message: will need to use a different port?
+localhost:3000 doesn't work: in terminal > copy this link and past into a browser window:
+http://192.168.1.187:3000
+
+****************************************************************************************************************************************************************/
+
+
