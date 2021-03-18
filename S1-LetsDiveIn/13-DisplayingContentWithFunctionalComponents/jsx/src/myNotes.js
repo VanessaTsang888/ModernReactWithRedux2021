eb8b112f189965928d7b06e2bac9e5ea5bd1d9b6
VanessaTsang888 <vantaidesign8@gmail.com>
2021-03-18 21:01:13 +0000

    Completed lecture 13

---
+++ S1-LetsDiveIn/13-DisplayingContentWithFunctionalComponents/jsx/src/myNotes.js
@@ -0,0 +1,9 @@
+/* MY NOTES:
+
+A component is a function or a class that produces HTML to show to the user (using JSX) -> and handles feedback from the user using Event Handlers.
+This lecture will focus on using JSX.
+
+
+
+*/
+
