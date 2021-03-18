f18bb3feca0c777334e76b3df11f005eab49a648
VanessaTsang888 <vantaidesign8@gmail.com>
2021-03-18 21:19:10 +0000

    Lecture 9 re-added

---
+++ S1-LetsDiveIn/9-Exploring-a-CreateReactApp-Project/jsx/myNotes.js
@@ -0,0 +1,28 @@
+/**************************************************************************************************************************************************************** 
+
+9. Exploring a Create-React-App Project:
+
+I create new fold named: jsx since this is about the jsx syntax.
+The files insdie this fold is auto generated.
+
+When I create a React App, by default I will get some files:
+
+
+src: stands for source > will not use any of the auto generated code inside of here.
+
+The nodes modules directory:
+A lot of files here and I don't need to go into here.
+
+The package.json files lists out all the dependancies my projects requires and some project configs inside of it. Over time I will be
+doing several updates to this package.json file.
+
+Package-lock.jason: records the exact version of all the dependencies that I installed in my project. Under the dependencies there
+are many number of records. 
+
+Readme file: tells me how to run this React App project.
+
+.gitignore file: a reference to the git version control system and lists out the different files and folders that git should ignor
+if it's going to be tracking my projet and maintaining a record of all the different changes we're making to our codebase.
+
+
+****************************************************************************************************************************************************************/
