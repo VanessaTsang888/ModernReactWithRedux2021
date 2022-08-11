38ccd4d8e5fedf5616ea421028b3958def249aa3
Vanessa <vantaidesign8@gmail.com>
2022-08-11 13:53:04 +0100

    Completed lecture 38

--- S3-communicating-with-props/38-component-reuse/components/notes-lect-38.md
+++ S3-communicating-with-props/38-component-reuse/components/notes-lect-38.md
@@ -1,3 +1,23 @@
 # 38 Component Reuse
 
-xx
+Part 2 of lecture 37: I have wrapped the remaining x2 CommentDetail component tag
+with Approval Card component tag. Now the approval card has the comment detail showing inside it.
+
+I feel that CommentDetail is now the grandchild and the ApprovalCard is still the child of App in this index.js file.
+
+After testing in the UI, I can see that all three comments are now being rendered inside of an instance of approval card. Basically, I've set up ApprovalCard to take some child in (use for anyother purpose I want) and that is how I can reuse the ApprovalCard component for all three comments that has different data that uses same styling.
+
+Inside ApprovalCard we don't have to pass in a React component. We can put in plain text or standard jsx as well. Tested in the UI -> Now a new card is returned at the top.
+
+Also multiple elements can be wrapped by the ApprovalCard component tag, e.g. I can wrap the plain text a <div> tag, and inside that a <h4> with plain text:
+
+```
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+```
+
+This will work ONLY if we make sure we reference { props.children } in the ApprovalCard component.
--- S3-communicating-with-props/38-component-reuse/components/src/index.js
+++ S3-communicating-with-props/38-component-reuse/components/src/index.js
@@ -1,12 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import faker from "faker";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+/*
+ Part 2 of lecture 37: I have wrapped the remaining x2 CommentDetail component tag
+ with Approval Card component tag. Now the approval card has the comment detail showing inside it.
+
+ I feel that CommentDetail is now the grand child, ApprovalCard is still the child of App in this index.js file.
+
+
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
-    <div className="ui container comments">
+    <div className='ui container comments'>
+      {/* Return a new card at the top. Inside ApprovalCard we don't have to pass in a React component. 
+      We can put in plain text or standard jsx as well. This will work ONLY if we make sure we reference 
+      { props.children } in the ApprovalCard component.
+      */}
       <ApprovalCard>
         <div>
           <h4>Warning!</h4>
@@ -16,27 +29,27 @@ const App = () => {
 
       <ApprovalCard>
         <CommentDetail
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          content="Nice blog post"
+          author='Sam'
+          timeAgo='Today at 4:45PM'
+          content='Vanessa, nice blog post!'
           avatar={faker.image.image()}
         />
       </ApprovalCard>
 
       <ApprovalCard>
         <CommentDetail
-          author="Alex"
-          timeAgo="Today at 2:00AM"
-          content="I like the subject"
+          author='Alex'
+          timeAgo='Today at 2:00AM'
+          content='Great subject on frontend development today.'
           avatar={faker.image.image()}
         />
       </ApprovalCard>
 
       <ApprovalCard>
         <CommentDetail
-          author="Jane"
-          timeAgo="Yesterday at 5:00PM"
-          content="I like the writing"
+          author='Jane'
+          timeAgo='Yesterday at 5:00PM'
+          content='I love your style of writing Vanessa :-)'
           avatar={faker.image.image()}
         />
       </ApprovalCard>
@@ -44,4 +57,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
