d53088e4c2eefd5697a4faeca837055de44e3fa0
Vanessa <vantaidesign8@gmail.com>
2022-08-16 17:03:43 +0100

    Completed lecture 139

--- s12-understanding-hooks-in-react/139-helper-function-in-function-components/notes-lect-139.md
+++ s12-understanding-hooks-in-react/139-helper-function-in-function-components/notes-lect-139.md
@@ -1,3 +1,33 @@
 # 139 Helper Functions in Function Components
 
-**_How to detect when user clicks on one of the titles_**
+**_The Task_**
+
+How to detect when user clicks on one on the titles?
+
+In the Accordion.js file, at the top of the Accordion component, write an Helper Function and store this in the variable: onTitleClick. This should take an argument of index since we need to track which title of the items array user has clicked on.
+
+```js
+const onTitleClick = (index) => {
+```
+
+So we want to call this Helper Function each time user clicks on the div with className of title active, we pass through the index that is inside of the onClick={()} Prop.
+
+Callback function
+
+```js
+onClick={() => onTitleClick(index)}
+```
+
+renderedItems function -> return statement -> div -> callback function:
+
+We don't want to run the callback function the instant our component is rendered. Instead we want to call it sometime in the future. To do this we wrap it with an arrow function. When the onClick arrow function is executed, run this onTitleClick function.
+
+I have tested in the UI using the Chrome Inspect tool and found the Helper Function works as expected. I've clicked on each title and in the Console tab I can see the console.log has printed out the Title clicked text along with the index of the title.
+
+```js
+console.log('Title clicked', index);
+```
+
+## Make use of Hooks
+
+In the next lecture, use of hooks state system to keep track of which of the elements should be expanded and collapsed.
--- s12-understanding-hooks-in-react/139-helper-function-in-function-components/widgets/src/components/Accordion.js
+++ s12-understanding-hooks-in-react/139-helper-function-in-function-components/widgets/src/components/Accordion.js
@@ -1,13 +1,19 @@
-/********************************************************************
-Step 1:
-
-xxx
-
-***********************************************************************/
+/*************************************************************************************
+To dectect when user has clicked on a title, we need a Helper Function at
+the top of the Accordion component. This Helper Function is called onTitleClick
+and will be stored into this variable that we've defined with const. We consol.log
+out to test in the UI.
+*************************************************************************************/
 
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  /************************************************************************************
+  Helper Function with index to track which title user has clicked on.
+  Once user has clicked on this function then the index gets passed through
+  to the onClick Prop. 
+**********************************************************************************/
+
   const onTitleClick = (index) => {
     console.log('Title clicked', index);
   };
@@ -15,6 +21,10 @@ const Accordion = ({ items }) => {
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
+        {/* onClick Prop is a callback function that we want to run not when our App 
+        is rendered but sometime in the future/whenever user clicks on the title - 
+        the onTitleClick variable. When the onClick arrow function is executed, run this
+        onTitleClick function. */}
         <div className='title active' onClick={() => onTitleClick(index)}>
           <i className='dropdown icon'></i>
           {item.title}
